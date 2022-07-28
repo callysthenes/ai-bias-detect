3a22542dc1044be7e1e76fb0388d219b6e892429
Pedro V <91435423+Callisthenes@users.noreply.github.com>
2022-07-29 00:57:00 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fairdetect_group_a',
+      version='0.20',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      url='https://github.com/Callisthenes/ai-bias-detect',
+      author='Team A',
+      author_email='pedrovesteban@student.ie.edu',
+      license='MIT',
+          packages=['fairdetect_group_a'],
+      zip_safe=False)
