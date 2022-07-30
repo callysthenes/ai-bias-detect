d7ba2688a3112cc9531da444312f1336944e7845
Pedro V <91435423+Callisthenes@users.noreply.github.com>
2022-07-30 20:29:34 +0200

    pipy files

---
+++ build/lib/fair_detect_group_a/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ai-bias-detect',
+      version='0.30',  # Development release
+      description='FairDetect Group A IE MBD part time assignment for Python II class',
+      url='https://github.com/Callisthenes/ai-bias-detect',
+      author='Team_A_MBD_2022_MBD',
+      author_email='pedrovesteban@student.ie.edu',
+      license='MIT',
+          packages=['fair_detect_group_a'],
+      zip_safe=False)
